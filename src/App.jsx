5cbf85c1f83c9c08b930d1b4eea08cb817b1bc1c
Suprim24 <suprimachary88@gmail.com>
2025-07-02 21:24:14 +0545

    updated stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react"
+
+  import User from "./user"
 const App=()=>{
   const [display,setDisplay]=useState(false)
   return(
     <>
     <h1>toggle in react</h1>
-    
-    {
+    <button onClick={()=>setDisplay(!display)}>Toogle</button>
+    {/* {
     display?<h1>Suprim</h1>:null
+    } */}
+    {
+    display?<h1><User/></h1>:null
     }
      
     </>
